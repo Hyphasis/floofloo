1f7bb32b7f540e3d8e5f81727d3e372192483af9
tienyulin <t.tienyulin@gmail.com>
2021-12-06 13:53:32 +0800

    refactor: remove useless route

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -6,7 +6,7 @@ require 'slim/include'
 
 module Floofloo
   # Web App
-  class App < Roda # rubocop:disable Metrics/ClassLength
+  class App < Roda
     plugin :halt
     plugin :flash
     plugin :all_verbs # recognizes HTTP verbs beyond GET/POST (e.g., DELETE)
@@ -25,57 +25,13 @@ module Floofloo
 
         view 'home', locals: { events_session: events_session }
       end
-      routing.on 'issue' do
-        routing.on String do |issue_name|
+      routing.on 'issue' do # rubocop:disable Metrics/BlockLength
+        routing.on String do |issue_name| # rubocop:disable Metrics/BlockLength
           routing.on 'event' do # rubocop:disable Metrics/BlockLength
             routing.on String do |event_name| # rubocop:disable Metrics/BlockLength
-              routing.on 'news' do # rubocop:disable Metrics/BlockLength
-                routing.is do # rubocop:disable Metrics/BlockLength
-                  # POST /event/{event_name}/news
-                  routing.post do
-                    language = routing.params['language']
-                    keywords = routing.params['keywords']
-                    from = routing.params['from']
-                    to = routing.params['to']
-                    sort_by = routing.params['sort_by']
-
-                    routing.halt 400 if keywords.nil?
-
-                    event = Floofloo::Entity::Event.new(
-                      id: nil,
-                      name: event_name
-                    )
-                    event_result = Repository::IssuesFor.entity(event).create(event)
-
-                    # Get news from News
-                    news = News::NewsMapper
-                      .new(App.config.NEWS_KEY)
-                      .find(language, keywords, from, to, sort_by)
-                    news_result = Repository::ArticlesFor.entity(news).create(event_result, news)
-
-                    # Put the news on screen (Home page)
-                    routing.redirect "/event/#{event_name}/news/#{news_result.id}"
-                  rescue StandardError => e
-                    flash[:error] = 'Failed to get news!'
-                    puts e.message
-
-                    routing.redirect '/'
-                  end
-
-                  routing.get do # rubocop:disable Metrics/BlockLength
-                    # GET /event/{event_name}/news/{news_id}
-                    routing.on String do |news_id|
-                      news = Repository::ArticlesFor.klass(Entity::News)
-                        .find_id(news_id)
-
-                      view 'news', locals: { news: news }
-                    rescue StandardError => e
-                      flash[:error] = 'Failed to get news!'
-                      puts e.message
-
-                      routing.redirect '/'
-                    end
-
+              routing.on 'news' do
+                routing.is do
+                  routing.get do
                     # GET /issue/{issue_name}/event/{event_name}/news
                     routing.is do
                       result = Forms::GetNews.new.call(issue: issue_name,
@@ -124,21 +80,6 @@ module Floofloo
                 rescue StandardError => e
                   flash[:error] = 'Failed to get donations!'
                   puts e.full_message
-
-                  routing.redirect '/'
-                end
-              end
-
-              routing.on 'delete' do
-                # GET /event/{event_name}/delete
-                routing.get do
-                  session[:keywords].delete(event_name)
-
-                  events_session = session[:keywords]
-                  view 'home', locals: { events_session: events_session }
-                rescue StandardError => e
-                  flash[:error] = 'Failed to delete event!'
-                  puts e.message
 
                   routing.redirect '/'
                 end
